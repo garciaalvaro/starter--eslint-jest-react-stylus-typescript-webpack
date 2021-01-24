30ac7b9b880d288b0cb220f4d39b48a91c4f88fb
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-01-24 12:45:00 +0100

    Fixed code format

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,9 @@
 module.exports = {
-	plugins: [
-		"@babel/plugin-transform-runtime",
-	],
+	plugins: ["@babel/plugin-transform-runtime"],
 
 	presets: [
 		"@babel/preset-env",
 		"@babel/preset-react",
 		"@babel/preset-typescript",
 	],
-}
+};
